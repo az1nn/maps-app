3de323cf419522797740b25c14d0d6a42d9b7299
az1nn <alansa015@gmail.com>
2023-06-27 20:28:30 -0300

    feat: map and form

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,120 @@
+import { useEffect, useReducer, useState } from 'react'
+
 import MapContainer from './Map/MapContainer'
+export interface IMark {
+  lat: number
+  long: number
+  text: string
+}
+export interface IFormMark {
+  lat: string
+  long: string
+  text: string
+}
+export interface FormAction {
+  field: string
+  payload: string
+}
 
 function App() {
+  function reducer(state: IMark[], action: IMark) {
+    return [...state, action]
+  }
+
+  const [state, dispatch] = useReducer(reducer, [])
+
+  function formReducer(state: IFormMark, action: FormAction) {
+    return {
+      ...state,
+      [action.field]: action.payload
+    }
+  }
+
+  const [formState, formDispatch] = useReducer(formReducer, {
+    lat: '',
+    long: '',
+    text: ''
+  })
+
   return (
-    <div className="h-[100vh] w-full bg-sky-700/80">
+    <div className="h-full min-h-[100vh] w-full bg-sky-700/80">
       <div>
         <div className="flex justify-center">
           <h1 className="p-4 text-3xl font-bold text-white">
             My Favorite Places in the World
           </h1>
         </div>
-        <MapContainer />
+        <MapContainer data={state} />
+        <div className="flex flex-col flex-wrap items-center">
+          <p className="pb-4 pt-6 text-xl font-bold text-white">
+            Add a new one:
+          </p>
+          <div className="flex w-full flex-row">
+            <div className="flex w-1/2 flex-col items-center">
+              <div className="my-4 flex w-1/2 justify-between">
+                <label htmlFor="lat" className="text-xl font-normal text-white">
+                  Lat:
+                </label>
+                <input
+                  id="lat"
+                  type="number"
+                  className="ml-4 w-2/3 rounded px-2"
+                  value={formState.lat}
+                  onChange={(e) => {
+                    formDispatch({ payload: e.target.value, field: 'lat' })
+                  }}
+                />
+              </div>
+              <div className="my-4 flex w-1/2 justify-between">
+                <label
+                  htmlFor="long"
+                  className="text-xl font-normal text-white"
+                >
+                  Long:
+                </label>
+                <input
+                  id="long"
+                  type="number"
+                  className="ml-4 w-2/3 rounded px-2"
+                  value={formState.long}
+                  onChange={(e) => {
+                    formDispatch({ payload: e.target.value, field: 'long' })
+                  }}
+                />
+              </div>
+            </div>
+            <div className="flex w-1/2 flex-col items-start">
+              <div className="my-4 flex w-2/3 justify-between">
+                <label
+                  htmlFor="text"
+                  className="text-xl font-normal text-white"
+                >
+                  Comments:
+                </label>
+                <textarea
+                  id="text"
+                  className="ml-4 h-24 w-2/3 rounded px-2"
+                  value={formState.text}
+                  onChange={(e) => {
+                    formDispatch({ payload: e.target.value, field: 'text' })
+                  }}
+                />
+              </div>
+            </div>
+          </div>
+          <button
+            className="mt-4 rounded border bg-white px-8 py-2 font-bold hover:scale-105"
+            onClick={() => {
+              dispatch({
+                lat: Number(formState.lat),
+                long: Number(formState.long),
+                text: formState.text
+              })
+            }}
+          >
+            OK
+          </button>
+        </div>
       </div>
     </div>
   )
--- src/components/Map/MapContainer.tsx
+++ src/components/Map/MapContainer.tsx
@@ -1,19 +1,56 @@
 import { useMemo } from 'react'
-import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api'
+import GoogleMapReact from 'google-map-react'
+import { BiSolidPin } from 'react-icons/bi'
+
+import { IMark } from 'components/App'
+
 import './Map.css'
 
-export default function MapContainer() {
-  const key = 'AIzaSyDxmJzqEDLoDCj7-BeRNjHmGkEiP5TV-ao'
-  const center = useMemo(() => ({ lat: 44, lng: -80 }), [])
-  const { isLoaded } = useLoadScript({
-    googleMapsApiKey: key
-  })
+export function MarkCard(props: any) {
+  const { text, lat, lng } = props
+  return (
+    <div>
+      <div className="mb-[-0.2em] ml-[-1.2em]">
+        <BiSolidPin size={30} color="#A52A2A" />
+      </div>
+      <div className="w-max max-w-xs rounded border bg-white p-[1em] text-[1.4em]">
+        <p>
+          <b>Notes: </b>
+          {text}
+        </p>
+        <div className="mt-4 flex flex-row justify-between pr-24">
+          <p>
+            <b>Lat: </b>
+            {lat}
+          </p>
+          <p>
+            <b>Long: </b>
+            {lng}
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
 
-  if (!isLoaded) return <div>Loading...</div>
+export default function MapContainer(props: { data: IMark[] }) {
+  const { data } = props
+  const key = import.meta.env.VITE_GMAPS_KEY
+  const center = useMemo(() => ({ lat: 0, lng: 0 }), [])
 
   return (
-    <GoogleMap zoom={10} center={center} mapContainerClassName="map-container">
-      <Marker position={center} />
-    </GoogleMap>
+    <div className="h-[50vh] w-full">
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: key }}
+        defaultCenter={center}
+        defaultZoom={0}
+      >
+        {data.map((item, i) => {
+          return (
+            <MarkCard key={i} text={item.text} lat={item.lat} lng={item.long} />
+          )
+        })}
+      </GoogleMapReact>
+    </div>
   )
 }
